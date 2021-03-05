4bdccaf49fa87f171dd7e57dc33988a94ca8ab24
Emma Blott <blott23@gmail.com>
2021-03-05 11:48:06 +0000

    test for undefined written

--- test/chat-bot.test.ts
+++ test/chat-bot.test.ts
@@ -5,4 +5,15 @@ describe('ChatBot', ()=> {
     expect(newChatState.chatState).toBe(0)
     done()
   }) 
+
+  it ("returns welcome state if undefined is passed in", async done => {
+    const bothUndefined = await getNextChatState(undefined, undefined)
+    expect(bothUndefined.chatState).toBe(0)
+    const responseUndefined = await getNextChatState(0,undefined)
+    expect(responseUndefined.chatState).toBe(0)
+    const chatStateUndefined = await getNextChatState(undefined, "hello")
+    expect(chatStateUndefined.chatState).toBe(0)
+    done()
+  })
+
 })
